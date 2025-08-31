18b506d4be9a56c10655059089229e4be6b02f45
Ivan Ristović <contact@ristovic.net>
2025-08-31 20:55:26 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,13 @@ set -e # stop executing after error
 echo "Starting Jekyll build"
 
 ####################################################
+# Set workspace permissions
+####################################################
+
+mkdir /workspace
+chmod -R a+w /workspace
+
+####################################################
 # Build the Jekyll site
 ####################################################
 
